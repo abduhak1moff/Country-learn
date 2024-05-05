caf807ef59161e63ecd3d7550e6a37ebf07d3187
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-05-05 21:12:06 +0500

    Country learn

---
+++ src/components/shares/Loading.jsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <div className="alert py-5 text-center alert-warning">Loading...</div>;
+};
+
+export default Loading;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,69 @@
+import { Alert, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+
+import CountryCard from "../components/card/CountryCard";
+import Loading from "../components/shares/Loading";
+
+import useFetchPagination from "../hooks/useFetchPagination";
+import { useState } from "react";
+
+const HomePage = () => {
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("default");
+
+  const url = search ? `name/${search}` : "all";
+
+  const params =
+    sort !== "default"
+      ? {
+          sort: "title",
+          order: sort,
+        }
+      : {};
+
+  const {
+    data: countries,
+    error,
+    loading,
+    pagination,
+  } = useFetchPagination(url, JSON.stringify(params));
+
+  return (
+    <Container>
+      <InputGroup className="my-3">
+        <Form.Control
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Searching countries"
+        />
+        <InputGroup.Text>
+          <Form.Select onChange={(e) => setSort(e.target.value)}>
+            <option value="default">Default</option>
+            <option value="asc">Asc</option>
+            <option value="desc">Desc</option>
+          </Form.Select>
+        </InputGroup.Text>
+      </InputGroup>
+      {error ? <Alert>Error</Alert> : null}
+      {loading ? (
+        <Loading />
+      ) : (
+        <Row xs={1} sm={2} md={3} lg={4}>
+          {countries.map((country) => (
+            <Col className="mb-3" key={country.name.official}>
+              <CountryCard
+                name={country.name.common}
+                population={country.population}
+                region={country.region}
+                capital={country.capital?.[0]}
+                image={country.flags.png}
+              />
+            </Col>
+          ))}
+        </Row>
+      )}
+      {pagination}
+    </Container>
+  );
+};
+
+export default HomePage;
